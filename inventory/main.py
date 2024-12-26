9e83c8739b4afc776df8dd884540f9d5af8544b3
dhatchu14 <dhatchani.manickam@gmail.com>
2024-12-26 10:55:40 +0530

    Configured CORS middleware to allow requests from frontend at http://localhost:3000 and enable all HTTP methods and headers.

--- inventory/main.py
+++ inventory/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 from redis_om import get_redis_connection, HashModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:3000'],
+    allow_methods=['*'],
+    allow_headers=['*']
+)
 
 # Correct Redis connection setup
 redis = get_redis_connection(
